ed4e7f14bfc17253398756e996500840d5b999fb
Ole Idar Bruvik <ole.bruvik@amesto.no>
2022-12-01 09:59:40 +0100

    First day, QD solution...

---
+++ 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+var result = new List<int>();
+
+var input = System.IO.File.ReadAllLines("./puzzleInput.txt");
+
+var accumulatedCalories = 0;
+
+foreach (var line in input)
+{
+    if (line.Length == 0)
+    {
+        result.Add(accumulatedCalories);
+        accumulatedCalories = 0;
+        continue;
+    }
+    accumulatedCalories += int.Parse(line);
+
+}
+if (accumulatedCalories > 0)
+{
+    result.Add(accumulatedCalories);
+}
+
+Console.WriteLine("Part 1:");
+Console.WriteLine(result.Max());
+
+result.Sort();
+Console.WriteLine("Part 2:");
+int resCount = result.Count;
+int result2 = 0;
+for (int i = 1; i < 4; i++)
+{
+    result2 += result[resCount - i];
+}
+
+Console.WriteLine(result2);
